0952da3a689e2e27330e41267c0ef4f6694b8064
Aleksey Kanev <alnikkanev@icloud.com>
2017-03-04 19:36:29 +0300

    CreateClass
    Created DetaleViewController

---
+++ HW2/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  HW2
+//
+//  Created by Aleksey Kanev on 04.03.17.
+//  Copyright © 2017 Aleksey Kanev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
